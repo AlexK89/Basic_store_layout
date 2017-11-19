b28ce7ffe9a651265f87d835e174489bfb6c3f0a
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-19 17:30:46 +0000

    layout sample

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var img_number = 0;
+var timer = null;
+var $items = $(".main_img_items").children();
+
+//create_dots - generate dots section
+function create_dots() {
+    var dot_block = "";
+    var $images_position = $items.length;
+    for (var i = 0; i < $images_position; i++) {
+        dot_block += "<li class='dots_navigation' data-img=" + i + ">.</li>"
+    }
+    $(".dots").append(dot_block);
+    $('.dots_navigation').eq(0).addClass("active");
+}
+
+//active_dot - change styling for chosen dot
+function active_dot() {
+    $(".active").removeClass("active");
+    $('.dots_navigation').eq(img_number).addClass("active");
+}
+
+function selected_img() {
+    clearTimeout(timer);
+    if ($items[img_number]) {
+        active_dot();
+        $($items[img_number]).addClass("active");
+    } else {
+        img_number = 0;
+        active_dot();
+        $($items[img_number]).addClass("active");
+    }
+    img_number++;
+    timer = setTimeout(selected_img, 3000);
+
+}
+
+function manual_change() {
+    document.querySelectorAll(".dots_navigation").forEach(function (value) {
+        value.addEventListener("click", function () {
+            img_number = parseInt($(this).attr("data-img"));
+            selected_img();
+        })
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    create_dots();
+    selected_img();
+    manual_change();
+});
